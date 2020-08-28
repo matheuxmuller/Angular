970cdc931c5d1d6805a381a559c44c65c1cce92c
matheuxmuller <matheuxmuller@gmail.com>
2020-08-28 15:53:06 -0300

    Implementação de Login e Cadastro

--- blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
+++ blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilLateralComponent implements OnInit {
 
+  nome: String;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.nome = 'Matheus Müller'
   }
 
 }
